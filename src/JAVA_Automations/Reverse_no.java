1fc7d216084feb347bb62c4560cdc736462d98d4
rahul13425 <rk6965490@gmail.com>
2024-05-15 10:15:23 +0530

    Update Reverse_no.java

--- src/JAVA_Automations/Reverse_no.java
+++ src/JAVA_Automations/Reverse_no.java
@@ -1,4 +1,16 @@
 package JAVA_Automations;
 
 public class Reverse_no {
+    public static void main(String[] args) {
+
+        int a= 102;
+        int reverse_number = 0;
+        while(a!=0)                     // in while loop a never be zero
+        {
+            int digit = a% 10;   // % gives us reminder
+            reverse_number = reverse_number*10 + digit;
+             a=a/10;    // (/) gives us quotient
+        }
+        System.out.println(reverse_number);
+    }
 }
