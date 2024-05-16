b2f3861fe3714bb19549d0092d6dab6d04cfdf1a
rahul13425 <rk6965490@gmail.com>
2024-05-16 12:05:50 +0530

    Done

---
+++ src/JAVA_Automations/ARRAYS/Remove_Duplicate_no.java
@@ -0,0 +1,4 @@
+package JAVA_Automations.ARRAYS;
+
+public class Remove_Duplicate_no {
+}
--- src/JAVA_Automations/ARRAYS/Reversal_Array.java
+++ src/JAVA_Automations/ARRAYS/Reversal_Array.java
@@ -8,7 +8,7 @@ public class Reversal_Array {
 
         int start = 0;
         int end = arr.length - 1;
-        
+
             System.out.println("The array before the reverse : ");
             for (int i = 0; i < arr.length; i++) {
                 System.out.println(arr[i]);
